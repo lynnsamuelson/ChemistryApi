ed7be21188eb94cdf7bff17d5c87b425ca8647ac
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-24 09:18:09 -0500

    wip .gitignore

---
+++ PeriodicTableApi/PeriodicTableApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PeriodicTableApi.WebApiApplication" Language="C#" %>
---
+++ PeriodicTableApi/PeriodicTableApi/Models/ElementContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PeriodicTableApi.Models
+{
+    public class ElementContext : DbContext
+    {
+        public DbSet<Element> Element { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Element>()
+                .ToTable("Element")
+                .HasKey(c => c.ElementId);
+
+        }
+
+    }
+}
